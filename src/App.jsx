068df101ed5e8d7843052f947cb67f22abcd77d5
Saelyeam <eileen4505@gachon.ac.kr>
2025-05-18 19:26:57 +0900

    feat:Storelist page 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Mypage from './pages/Mypage';
+import StoreList from './pages/StoreList';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/mypage" element={<Mypage />} />
+      <Route path="/stores" element={<StoreList />} />
     </Routes>
   );
 }
