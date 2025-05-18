3637c0d488eaa46e0853830cbd23309dbcaab2d9
Saelyeam <eileen4505@gachon.ac.kr>
2025-05-18 20:32:23 +0900

    feat:Mecuselect 화면 UI구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Mypage from './pages/Mypage';
+<<<<<<< Updated upstream
+=======
+import StoreList from './pages/StoreList';
+import MenuSelect from './pages/MenuSelect';
+>>>>>>> Stashed changes
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/mypage" element={<Mypage />} />
+<<<<<<< Updated upstream
+=======
+      <Route path="/stores" element={<StoreList />} />
+      <Route path="/menu-select" element={<MenuSelect />} />
+
+>>>>>>> Stashed changes
     </Routes>
   );
 }
