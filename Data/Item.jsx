4bb0bdbffd4a5e6aa9520ad7c9be2d6bfb0973d1
jung-minwoo <phansha23@gmail.com>
2025-05-19 13:59:47 +0900

    Add files via upload

---
+++ Data/Item.jsx
@@ -0,0 +1,44 @@
+let Item = [
+    {
+        id: 0,
+        title: 'Title 1',
+        location: 'Location 1',
+        price: 20000,
+        time: 30
+    },
+    
+    {
+        id: 1,
+        title: 'Title 2',
+        location: 'Location 2',
+        price: 30000,
+        time: 20
+    },
+    
+    {
+        id: 2,
+        title: 'Title 3',
+        location: 'Location 3',
+        price: 12000,
+        time: 55
+    },
+
+    {
+        id: 3,
+        title: 'Title 4',
+        location: 'Location 4',
+        price: 14000,
+        time: 15
+    },
+
+    {
+        id: 4,
+        title: 'Title 5',
+        location: 'Location 5',
+        price: 30000,
+        time: 20
+    },
+
+];
+
+export default Item;
