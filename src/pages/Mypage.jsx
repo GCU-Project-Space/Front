65087f78bb55221301d3cb7367a8f6e9c8bf87d0
Saelyeam <eileen4505@gachon.ac.kr>
2025-06-04 12:23:50 +0900

    fix : MYPAGE 로그아웃버튼 생성

--- src/pages/Mypage.jsx
+++ src/pages/Mypage.jsx
@@ -5,9 +5,25 @@ import FixedLayout from '../components/FixedLayout';
 import Header from '../components/Header';
 import UserInfo from '../components/UserInfo';
 import UserInfoBox from '../components/UserInfoBox';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function Mypage() {
   const navigate = useNavigate();
+
+    const handleLogout = () => {
+    const confirmed = window.confirm("로그아웃하시겠습니까?");
+    if (confirmed) {
+      // localStorage.clear(); // 필요 시 로그인 정보 제거
+      
+      toast.success('로그아웃 되었습니다.', {
+        position: 'bottom-center',
+        autoClose: 2000,
+      });
+      setTimeout(() => navigate('/'), 500); // 2초 후 홈으로 이동
+    }
+  };
+
   return (
     <FixedLayout>
       <Header />
@@ -39,6 +55,24 @@ function Mypage() {
             <span>최근 주문</span>
             <span style={{ fontSize: '18px', color: 'black',fontWeight: '500' }} onClick={() => window.location.href = '/history'}>{'>'}</span>
           </div>
+        </div>
+        <div style={{ marginBottom: '80px' }}>
+          <button
+            onClick={handleLogout}
+            style={{
+              width: '100%',
+              padding: '12px',
+              background: '#eeeeee',
+              color: 'black',
+              fontSize: '16px',
+              fontWeight: 'bold',
+              border: 'none',
+              borderRadius: '8px',
+              cursor: 'pointer',
+            }}
+          >
+            로그아웃
+          </button>
         </div>
       </main>
       <BottomNav />
